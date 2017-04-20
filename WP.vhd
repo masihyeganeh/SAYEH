8199bfc30f837572548a7879c32d8cc9c21f7e6c
Masih Yeganeh <goodboy.php@gmail.com>
2017-04-20 12:03:17 +0430

    wp done

--- WP.vhd
+++ WP.vhd
@@ -1,22 +1,29 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity WP is
     port (
         clk     : in std_logic;
         WPadd   : in std_logic;
         WPreset : in std_logic;
-        WPin    : in std_logic_vector(4 downto 0);
+        WPin    : in std_logic_vector(5 downto 0);
         WPout   : out std_logic_vector(5 downto 0)
     );
 end WP;
 
 architecture behavioral of WP is
+    signal pointTo : std_logic_vector(5 downto 0);
 begin
     process (clk)
     begin
         if clk'Event and clk ='1' then
-            -- implement it
+            if WPreset = '1' then
+                pointTo <= "000000";
+            elsif WPadd = '1' then
+                pointTo <= std_logic_vector(unsigned(pointTo) + unsigned(WPin));
+            end if ;
+            WPout <= pointTo;
         end if;
     end process;
 
